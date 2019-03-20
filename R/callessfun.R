98e62dc0ab92a16a253b88d8e767c0b9bd15af05
Rui Yang <ruiyang@broadinstitute.org>
2019-03-20 14:16:19 -0400

    modify function

--- R/callessfun.R
+++ R/callessfun.R
@@ -27,20 +27,20 @@ callessfun<-function(file_location,usable_tally_list,parameter_list){
     lp=para[[x]]$lp[which(para[[x]]$cvm==min(para[[x]]$cvm))]
     sigma=para[[x]]$sigma[which(para[[x]]$cvm==min(para[[x]]$cvm))]
 
-    ngene<-totdata %>% ungroup() %>% group_by(Nta) %>% summarise(ngene=n())
+    ngenedf<-totdata %>% ungroup() %>% group_by(Nta) %>% summarise(ngene=n()) %>% ungroup()
 
-    plist<-lapply(seq_len(length(ngene$Nta)),function(x){
+    plist<-lapply(seq_len(length(ngenedf$Nta)),function(x){
       lbd1<-1/rlnorm(10000, meanlog = lp, sdlog = sigma)
-      Zg1<-rnbinom(1000000,ngene$Nta[x],lbd1)
-      Zg2<-rnbinom(1000000,ngene$Nta[x],0.7)
-      subtot<-totdata %>% dplyr::filter(Nta==ngene$Nta[x])
+      Zg1<-rnbinom(1000000,ngenedf$Nta[x],lbd1)
+      Zg2<-rnbinom(1000000,ngenedf$Nta[x],0.7)
+      subtot<-totdata %>% dplyr::filter(Nta==ngenedf$Nta[x])
       subtot2<-subtot %>% rowwise() %>%
         mutate(pv1=length(which(Zg1<=gtot))/length(Zg1),
                pv2=length(which(Zg2>=gtot))/length(Zg2)) #pv2: the larger the better
       subtot3<-subtot2
       x=subtot3
     })
-    names(plist)<-paste("Nta",ngene$Nta,sep="_")
+    names(plist)<-paste("Nta",ngenedf$Nta,sep="_")
     pdata1<-do.call("rbind",plist)
 
     #adjusted p-value
